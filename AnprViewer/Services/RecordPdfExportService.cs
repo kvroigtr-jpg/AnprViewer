78ab8d509962929e7597f38dce990f79acd6fcbe
kvroigtr-jpg <kvroigtr@gmail.com>
2026-05-21 16:34:48 +0200

    Delete AnprViewer directory

--- AnprViewer/Services/RecordPdfExportService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using AnprViewer.Models;
-
-namespace AnprViewer.Services;
-
-public interface IRecordPdfExportService
-{
-    Task ExportRecordAsync(HistoricoRecord record, string filePath);
-}
